3ecd336bf8a76ff2d8994dd3313f2bdfa64d1a51
Jessyca Moraes <jessyca.moraes@bemol.local>
2020-03-26 14:36:59 -0400

    origin

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; //importa arquivo App.js
-
-ReactDOM.render(<App />, document.getElementById('root')); //coloca componente App dentro da div com id root
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://localhost:3333', //parte da url que vai ser mantida em todas as chamadas
-})
-
-export default api; //assim os outros arquivos conseguem importar este arquivo
