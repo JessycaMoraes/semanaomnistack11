5c7c95f2a9cffc13d9f790f324494de0f824941a
Jessyca Moraes <jessyca.moraes@bemol.local>
2020-03-27 19:28:39 -0400

    atual

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes'; //maiúsculo pois é componente
+
+function App() {
+  return (
+      <Routes />
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; //importa arquivo App.js
+
+ReactDOM.render(<App />, document.getElementById('root')); //coloca componente App dentro da div com id root
